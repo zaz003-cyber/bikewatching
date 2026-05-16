295ccdada79cad31f0f74c1109e806a885a1747a
Zihan Zhang <zaz003@ucsd.edu>
2026-05-15 17:39:25 -0700

    Update map.js

--- map.js
+++ map.js
@@ -12,3 +12,35 @@ const map = new mapboxgl.Map({
   minZoom: 10,
   maxZoom: 18,
 });
+
+const bikeLaneStyle = {
+  'line-color': '#32D400',
+  'line-width': 4,
+  'line-opacity': 0.6,
+};
+
+map.on('load', () => {
+  map.addSource('boston-bike-lanes', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+  });
+
+  map.addLayer({
+    id: 'boston-bike-lanes',
+    type: 'line',
+    source: 'boston-bike-lanes',
+    paint: bikeLaneStyle,
+  });
+
+  map.addSource('cambridge-bike-lanes', {
+    type: 'geojson',
+    data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
+  });
+
+  map.addLayer({
+    id: 'cambridge-bike-lanes',
+    type: 'line',
+    source: 'cambridge-bike-lanes',
+    paint: bikeLaneStyle,
+  });
+});
